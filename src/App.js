f257edf965d9ee1f8c546106d31d1242efddebbe
Cameron <77825507+CameronWDavis@users.noreply.github.com>
2024-01-06 13:14:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+//@author Cameron Davis
+//Last updated 1/6/2024
 
-function App() {
+/*
+This is a React app made to demonstrait use of state and props along with componennt managmenet by using a Bill Splitter app
+The bill splitter can add a person who when selected you can select them to figure out how the bill should be split and how much they owe you or you owe them.
+*/
+
+import { useState } from "react";
+import { FriendsList } from "./FriendsList";
+import { FormAddFriend } from "./FormAddFriend";
+import { FormSplitBill } from "./FormSplitBill";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export function Button({ children, onClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
-export default App;
+export function App() {
+  // State variables
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  // Toggle the visibility of the add friend form
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  // Add a new friend to the list
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  // Handle friend selection and toggle the selection
+  function handleSelection(friend) {
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
+  }
+
+  // Update the balance when splitting the bill
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
+
+  // Render the main application
+  return (
+    <div className="app">
+      <div className="sidebar">
+        {/* Display the list of friends */}
+        <FriendsList
+          friends={friends}
+          selectedFriend={selectedFriend}
+          onSelection={handleSelection}
+        />
+
+        {/* Display the "Add friend" form if the corresponding state is true */}
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+        {/* Button to toggle the visibility of the "Add friend" form */}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+
+      {/* Display the "Split bill" form if a friend is selected */}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
+      )}
+    </div>
+  );
+}
